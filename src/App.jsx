5980f03126f4a9092bc09ad87e7252fb59de3317
akzhol0 <ak.akzhol1603@gmail.com>
2024-11-21 19:28:13 +0500

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import "./assets/styles/global.css";
+import Marquee from "./components/marquee/Marquee.jsx";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-lg">Hello World!</h1>
+    <div className="w-full flex flex-col justify-center items-center">
+      <Header />
+      <div className="w-full h-[1000px] flex flex-col justify-start items-center">
+        <div className="w-full h-[200px] bg-[#131313] text-white overflow-hidden">
+          <Marquee />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/marquee/Marquee.jsx
@@ -0,0 +1,60 @@
+const Marquee = () => {
+  return (
+    <div className="h-full gg overflow-hidden flex gap-[0px] items-center">
+      <ul className="min-w-[180%] flex justify-between items-center">
+        <li>
+          <img width={100} height={100} src="/images/3.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/4.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/5.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/6.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/7.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/8.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/9.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/10.png" alt="logo" />
+        </li>
+      </ul>
+      <ul className="min-w-[180%] flex justify-between items-center">
+        <li>
+          <img width={100} height={100} src="/images/1.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/2.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/3.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/4.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/5.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/6.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/7.png" alt="logo" />
+        </li>
+        <li>
+          <img width={100} height={100} src="/images/8.png" alt="logo" />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Marquee;
